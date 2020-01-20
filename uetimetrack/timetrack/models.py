8924a909cceaea9c0a16a926938044392d986949
Jared Holzmeyer <jh642@evansville.edu>
2020-01-20 15:59:23 -0600

    Made a model for time entries. Have not done anything with it such as trying to assign a time entry to a user

--- uetimetrack/timetrack/models.py
+++ uetimetrack/timetrack/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
 
 # Create your models here.
+class TimeEntry(models.Model):
+    # Username of the student that submitted the entry
+    # username_submitted = models.CharField(max_length=100)
+    username_submitted = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    # The status of the entry
+    status = (
+        'Submitted',
+        'Denied',
+        'Approved',
+    )
+
+    # Username of the student leader that reviewed the entry
+    username_student_leader = models.CharField(max_length=100)
+
+    # The date the entry was submitted
+    # date_submitted = models.DateField()
+    date_submitted = date.today()
+
+    # The time listed for the specific time entry
+    time_entered = models.TimeField()
+
