91fd1de1ede70b4499ff5ab8a7fa4def22d19f42
Jared Holzmeyer <jh642@evansville.edu>
2020-01-21 09:50:32 -0600

    Created pages for each user level. Make accounts for each and logging in to them redirects to the specific page

--- uetimetrack/timetrack/urls.py
+++ uetimetrack/timetrack/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from timetrack import views
 
 urlpatterns = [
-    path('', views.default_login, name='index'),
+    path('', views.template_login, name='index'),
     path('hello/', views.hello_world),
 ]
